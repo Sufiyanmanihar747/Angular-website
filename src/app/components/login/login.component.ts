64952f91f763381df7aec8d60cc83a1971d3f1c3
sufiyan@manihar <sufiyanmanihar52@gmail.com>
2024-06-11 21:12:49 +0530

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email = new FormControl("", [
+    Validators.required,
+    Validators.email
+  ])
 
+  password = new FormControl("",[
+    Validators.required,
+    Validators.minLength(6)
+  ])
+
+  loginForm = new FormGroup({
+    email:this.email,
+    password:this.password
+  })
+
+  login(){
+    console.log(this.email.value)
+    console.log(this.password.value)
+  }
+
+  reset(){
+    this.loginForm.reset()
+  }
 }
